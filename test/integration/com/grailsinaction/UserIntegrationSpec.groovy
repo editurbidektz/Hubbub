8e499a4fefab8867f79a1ed2e66da5af01e17e66
Eduardo <editurbide@kaitzen-solutions.com>
2016-02-12 09:47:11 -0300

    ejercicio: 3.2.2 - Updating user properties

--- test/integration/com/grailsinaction/UserIntegrationSpec.groovy
+++ test/integration/com/grailsinaction/UserIntegrationSpec.groovy
@@ -13,12 +13,28 @@ class UserIntegrationSpec extends Specification {
         def joe = new User(loginId: 'joe', password: 'secret',
                         homepage: 'http://www.grailsinaction.com')
 
-        when: "the user is saved"
+        when: "The user is saved"
         joe.save()
 
-        then: "it saved successfully and can be found in the database"
+        then: "It saved successfully and can be found in the database"
         joe.errors.errorCount == 0
         joe.id != null
         User.get(joe.id).loginId == joe.loginId
+    }
+
+    def "Updating a saved user changes its properties"() {
+
+        given: "An existing user"
+        def existingUser = new User(loginId: 'joe', password: 'secret',
+                        homepage: 'http://www.grailsinaction.com')
+        existingUser.save(failOnError: true)
+
+        when: "A property is changed"
+        def foundUser = User.get(existingUser.id)
+        foundUser.password = 'sesame'
+        foundUser.save(failOnError: true)
+
+        then: "The change is reflected in the database"
+        User.get(existingUser.id).password == 'sesame'
     }
 }
